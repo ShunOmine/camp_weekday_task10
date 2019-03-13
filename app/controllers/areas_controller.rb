b16ebebc95bafb0aa9908b01daa9284ec682c886
ShunOmine <shun12baseball@gmail.com>
2019-03-13 13:48:43 +0900

    day10 ~edit view,controllerの編集

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -9,16 +9,11 @@ class AreasController < ApplicationController
 
   def search
     @area = Area.new
-    zipcode = params[:zipcode]
-    uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?zipcode=#{zipcode}")
-    response = Net::HTTP.get_response(uri)
-    @json = JSON.parse(response.body)
-    if @json["status"].to_i != 200
-      flash.now[:alert] = "#{@json["message"]}"
-    elsif @json["status"].to_i == 200
-      @query = uri.query
-      response = Net::HTTP.get_response(uri)
+    if params[:zipcode]
       begin
+        uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?zipcode=#{params[:zipcode]}")
+        @query = uri.query
+        response = Net::HTTP.get_response(uri)
         # responseの値に応じて処理を分ける
         case response
           # 成功した場合
@@ -27,49 +22,40 @@ class AreasController < ApplicationController
           @result = JSON.parse(response.body)
           # 表示用の変数に結果を格納
           @zipcode = @result["results"][0]["zipcode"]
-          @address1 = @result["results"][0]["address1"]
-          @kana1 = @result["results"][0]["kana1"]
-          @address2 = @result["results"][0]["address2"]
-          @kana2 = @result["results"][0]["kana2"]
-          @address3 = @result["results"][0]["address3"]
-          @kana3 = @result["results"][0]["kana3"]
           @prefcode = @result["results"][0]["prefcode"]
-          flash.now[:notice] = "地域が見つかりました。"
-          # 別のURLに飛ばされた場合
-        when Net::HTTPRedirection
-          @message = "Redirection: code=#{response.code} message=#{response.message}"
-          # その他エラー
-        else
-          @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+          @address1 = @result["results"][0]["address1"]
+          @address2 = @result["results"][0]["address2"]
+          @address3 = @result["results"][0]["address3"]
+          @kana1 = @result["results"][0]["kana1"]
+          @kana2 = @result["results"][0]["kana2"]
+          @kana3 = @result["results"][0]["kana3"]
         end
           # エラー時処理
-      rescue IOError => e
-        @message = "e.message"
-      rescue TimeoutError => e
-        @message = "e.message"
-      rescue JSON::ParserError => e
-        @message = "e.message"
-      rescue => e
-        @message = "e.message"
+      rescue IOError
+        flash.now[:alert] = @result["message"]
+      rescue TimeoutError
+        flash.now[:alert] = @result["message"]
+      rescue JSON::ParserError
+        flash.now[:alert] = @result["message"]
+      rescue
+        flash.now[:alert] = @result["message"]
       end
     end
   end
 
   def create
     @area = Area.new(set_params)
-    respond_to do |format|
-      if @area.save
-        format.html { redirect_to root_path, notice: '地域を登録しました。' }
-        format.json { render :index, status: :created, location: @task }
-      else
-        format.html { render :search, alert: "エラーが発生しました。" }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @area.save
+      flash[:notice] = "地域を登録しました！"
+      redirect_to root_path
+    else
+      flash[:alert] = "Validation failed: #{@area.errors.full_messages.join}"
+      redirect_to "/areas/search?zipcode=#{params[:area][:zipcode]}"
     end
   end
 
   private
     def set_params
-      params.require(:area).permit(:zipcode, :prefcode, :address1, :address2, :address3, :kana1, :kana2, :kana3, :introduction)
+      params.require(:area).permit!
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  resources :areas do
-    collection do
-      get 'search'
-      post 'search'
-    end
-  end
   root 'areas#index'
+  get '/areas/search', to: "areas#search"
+  post '/areas/search', to: "areas#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
